70e5c2759d81020f4a193742ea6c5f721f7f4003
Cocco <remo@a45e60d6bd83.ant.amazon.com>
2015-08-09 23:43:35 -0700

    Cleanup for p14 in go.

--- Problem14.go
+++ Problem14.go
@@ -14,7 +14,8 @@ func main() {
   var max_sequence_size, answer = 0, 1
     for i := 1; i <= 1000000; i++ {
       var sequence_element = i
-        var sequence_size = 1;
+      var sequence_size = 1;
+
       for sequence_element != 1 {
         sequence_size++
           sequence_element = next_collatz_number(sequence_element)
