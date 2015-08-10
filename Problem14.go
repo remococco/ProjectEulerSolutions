760c435176e726c3576fd4164be97f2d7acccdd7
Cocco <remo@a45e60d6bd83.ant.amazon.com>
2015-08-09 23:45:06 -0700

    I am not good at this.

--- Problem14.go
+++ Problem14.go
@@ -12,19 +12,19 @@ func next_collatz_number(n int) int {
 
 func main() {
   var max_sequence_size, answer = 0, 1
-    for i := 1; i <= 1000000; i++ {
-      var sequence_element = i
-      var sequence_size = 1;
+  for i := 1; i <= 1000000; i++ {
+    var sequence_element = i
+    var sequence_size = 1;
 
-      for sequence_element != 1 {
-        sequence_size++
-        sequence_element = next_collatz_number(sequence_element)
-      }
-
-      if sequence_size > max_sequence_size {
-        max_sequence_size = sequence_size
-        answer = i
-      }
+    for sequence_element != 1 {
+      sequence_size++
+      sequence_element = next_collatz_number(sequence_element)
     }
+
+    if sequence_size > max_sequence_size {
+      max_sequence_size = sequence_size
+      answer = i
+    }
+  }
   fmt.Printf("answer is %d with size %d\n", answer, max_sequence_size)
 }
